258ec77fb32922c5730cf27dd95ba741f9bf0a3c
Fadjrin Kurnia <fadjrin@gmail.com>
2021-05-24 09:06:11 +0700

    chat server instanXpert

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
 const express = require("express");
 var http = require("http");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 var server = http.createServer(app);
-var io = require("socket.io")(server);
+var io = require("socket.io")(server, {
+  cors: {
+    // origin: "http://localhost:8080",
+    origin: '*'
+  },
+});
 
 //middlewre
 app.use(express.json());
 var clients = {};
 
 io.on("connection", (socket) => {
-  console.log("connetetd");
   console.log(socket.id, "has joined");
   socket.on("signin", (id) => {
-    console.log(id);
     clients[id] = socket;
-    console.log(clients);
   });
+
   socket.on("message", (msg) => {
     console.log(msg);
     let targetId = msg.targetId;
+    msg['time'] = Date.now();
     if (clients[targetId]) clients[targetId].emit("message", msg);
   });
+
+  socket.on('disconnect', () => {
+    console.log('client disconnect...', socket)
+  })
 });
 
-server.listen(port, "0.0.0.0", () => {
-  console.log("server started");
-});
+server.listen(port, "0.0.0.0", { perMessageDeflate: false });
